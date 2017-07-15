5a8f9ade2fb30a10e621b4a23a3354d89c2be742
mukeshmali9976 <mukeshmali9976@gmail.com>
2017-07-15 16:33:09 +0530

    DB Changes

--- app/src/main/java/com/techmali/smartteam/database/DbParams.java
+++ app/src/main/java/com/techmali/smartteam/database/DbParams.java
@@ -17,6 +17,7 @@ public class DbParams {
     public static final String TBL_SECURITY_MENU_CONTROLLERS_LINK = "security_menu_controllers_link";
     public static final String TBL_LEAVE = "leave";
     public static final String TBL_ROLE = "role";
+    public static final String TBL_MESSAGE = "message";
     public static final String TBL_SECURITY_ACTION_USER_PERMISSION = "security_action_user_permission";
 
     /////////////////////
@@ -64,6 +65,28 @@ public class DbParams {
     static final String CLM_START_DATE = "start_date";
     static final String CLM_END_DATE = "end_date";
 
+    // Message
+    static final String CLM_MESSAGE_ID = "message_id";
+    static final String CLM_TICKET_NUMBER = "ticket_number";
+    static final String CLM_AUTHOR_ID = "author_id";
+    static final String CLM_RECEIPT_ID = "recipient_id";
+    static final String CLM_SUBJECT = "subject";
+    static final String CLM_FIRST_NAME = "first_name";
+    static final String CLM_LAST_NAME = "last_name";
+    static final String CLM_THREAD_ID = "thread_id";
+    static final String CLM_CUSTOM_FIELD = "custom_field";
+    static final String CLM_ACTIVITY_TYPE_ID = "activity_type_id";
+    static final String CLM_REF_TABLE = "ref_table";
+    static final String CLM_REF_ID = "ref_id";
+    static final String CLM_REF_CODE = "ref_code";
+    static final String CLM_IS_READ = "is_read";
+    static final String CLM_SYSTEM_GENERATED = "system_generated";
+    static final String CLM_ACCEPTED_SEND_TIME = "accepted_send_time";
+    static final String CLM_IS_SEND = "is_send";
+    static final String CLM_IN_QUEUE = "in_queue";
+    static final String CLM_DEVICE_TYPE = "device_type";
+    static final String CLM_DEVICE_TOKEN = "device_token";
+
     //Setting
     static final String CLM_SETTING_ID = "setting_id";
     static final String CLM_ADMIN_EMAIL = "admin_email";
@@ -80,7 +103,7 @@ public class DbParams {
 
 
     // Role table
-    static final String CLM__ROLE_ID = "role_id";
+    static final String CLM_ROLE_ID = "role_id";
     static final String CLM_SORT_ORDER = "sort_order";
     static final String CLM_ROLE_GROUP = "role_group";
     static final String CLM_IS_CUSTOM = "is_custom";
@@ -116,7 +139,7 @@ public class DbParams {
     ////// CREATE TABLE QUERY....
     ///////////////////
     /*
-        setting_value_type : 1=> test, 2 => json text
+        setting_value_type : 1=> test, 2 => json VARCHAR
         app_type : 0=> Both, 1=>user, 2=> driver
     */
     static final String CREATE_TBL_APP_SETTING = "CREATE TABLE " + TBL_APP_SETTING + "(" +
@@ -129,7 +152,7 @@ public class DbParams {
     /*
         thumb: Company Logo image
         admin_user_id: Ref to Admin user account which is associated with user
-        status_id: 1-Active,2-Inactive,3-Deleted,4-Pending Activation
+        status_id: 1-Active, 2-Inactive, 3-Deleted, 4-Pending Activation
      */
     static final String CREATE_TBL_COMPANY = "CREATE TABLE " + TBL_COMPANY + "(" +
             CLM_LOCAL_COMPANY_ID + " VARCHAR," +
@@ -147,9 +170,13 @@ public class DbParams {
             CLM_UPDATED_BY + " INTEGER," +
             CLM_UPDATED_ON + " DATETIME DEFAULT (CURRENT_TIMESTAMP))";
 
+    /*
+        payout_status : '1=> Unpaid, 2=> paid',            DEFAULT '1'
+        status_id : '1=> pending, 2=> approved, 3=> reject, 4=> delete',
+     */
     static final String CREATE_TBL_EXPENSE = "CREATE TABLE " + TBL_EXPENSE + " (" +
-            CLM_LOCAL_EXPENSE_ID + " VARCHAR," +
-            CLM_SERVER_EXPENSE_ID + " INTEGER, " +
+            CLM_LOCAL_EXPENSE_ID + " VARCHAR PRIMARY KEY," +
+            CLM_SERVER_EXPENSE_ID + " VARCHAR, " +
             CLM_COMPANY_ID + " INTEGER, " +
             CLM_USER_ID + " INTEGER, " +
             CLM_TASK_ID + " INTEGER, " +
@@ -162,6 +189,67 @@ public class DbParams {
             CLM_CREATED_ON + " DATETIME DEFAULT (CURRENT_TIMESTAMP)," +
             CLM_UPDATED_BY + " INTEGER," +
             CLM_UPDATED_ON + " DATETIME DEFAULT (CURRENT_TIMESTAMP))";
+
+    /*
+          status_id : '1=>pending, 2=> approved, 3=> reject',         DEFAULT '1'
+          is_delete : '0 => not delete, 1=> deleted',                 DEFAULT '0'
+    */
+    static final String CREATE_TBL_LEAVE = "CREATE TABLE " + TBL_LEAVE + "(" +
+            CLM_LOCAL_LEAVE_ID + " VARCHAR PRIMARY KEY," +
+            CLM_SERVER_LEAVE_ID + " VARCHAR," +
+            CLM_COMPANY_ID + " INTEGER," +
+            CLM_USER_ID + " INTEGER," +
+            CLM_START_DATE + "  VARCHAR," +
+            CLM_END_DATE + "  VARCHAR," +
+            CLM_STATUS_ID + " INTEGER," +
+            CLM_IS_DELETE + " INTEGER," +
+            CLM_CREATED_BY + " INTEGER," +
+            CLM_CREATED_ON + " DATETIME DEFAULT (CURRENT_TIMESTAMP)," +
+            CLM_UPDATED_BY + " INTEGER," +
+            CLM_UPDATED_ON + " DATETIME DEFAULT (CURRENT_TIMESTAMP))";
+
+    /*
+           author_id : 'Ref to user table, who raised this ticket',
+           recipient_id : 'Ref to user table, who has received the message',
+           thread_id : 'If ticket is refering to reply of previous ticket, can be null, content parant message id ',
+           activity_type_id :'Ref to ActivityTypeID - TODO-- Ref to message_type tbable 1-Normal Message,2-Push,3-Email,4-Ticket,5-Booking Request,6-Staff Assignment,7-Booking Status Notification  8 - Accepr Reject Notification',
+           ref_id : 'refer to original table from where this message is generated'
+           status_id : 1=> active, 3=> Delete (Other status will not require)             DEFAULT '1'
+           is_read : DEFAULT '0',
+           system_generated : 1=> Yes, 0=> No                                  DEFAULT '0'
+           accepted_send_time` : timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'future notification',
+           is_send : DEFAULT '0',
+           in_queue : DEFAULT '0',
+           device_type : 1-iOS, 2-Android, 3-Web Admin
+     */
+    static final String CREATE_TBL_MESSAGE = "CREATE TABLE " + TBL_MESSAGE + " (" +
+            CLM_MESSAGE_ID + " INTEGER PRIMARY KEY," +
+            CLM_TICKET_NUMBER + " VARCHAR," +
+            CLM_AUTHOR_ID + " VARCHAR," +
+            CLM_RECEIPT_ID + " VARCHAR," +
+            CLM_SUBJECT + " VARCHAR," +
+            CLM_DESCRIPTION + " VARCHAR," +
+            CLM_FIRST_NAME + " VARCHAR," +
+            CLM_LAST_NAME + " VARCHAR," +
+            CLM_THREAD_ID + " VARCHAR," +
+            CLM_CUSTOM_FIELD + " VARCHAR," +
+            CLM_ACTIVITY_TYPE_ID + " VARCHAR," +
+            CLM_REF_TABLE + " VARCHAR," +
+            CLM_REF_ID + " VARCHAR," +
+            CLM_REF_CODE + " VARCHAR," +
+            CLM_STATUS_ID + " VARCHAR," +
+            CLM_IS_READ + " INTEGER DEFAULT 0," +
+            CLM_SYSTEM_GENERATED + " INTEGER DEFAULT 0," +
+            CLM_ACCEPTED_SEND_TIME + " DATETIME DEFAULT (CURRENT_TIMESTAMP)," +
+            CLM_IS_SEND + " INTEGER DEFAULT 0," +
+            CLM_IN_QUEUE + " INTEGER DEFAULT 0," +
+            CLM_DEVICE_TYPE + " INTEGER DEFAULT 2," +
+            CLM_DEVICE_TOKEN + " VARCHAR," +
+            CLM_CREATED_BY + " VARCHAR," +
+            CLM_UPDATED_BY + " VARCHAR," +
+            CLM_CREATED + " DATETIME DEFAULT (CURRENT_TIMESTAMP)," +
+            CLM_UPDATED + " DATETIME DEFAULT (CURRENT_TIMESTAMP))";
+
 
     static final String CREATE_TBL_TASK = "CREATE TABLE " + TBL_TASK + "(" +
             CLM_LOCAL_TASK_ID + " VARCHAR," +
@@ -179,7 +267,7 @@ public class DbParams {
             CLM_UPDATED_ON + " DATETIME DEFAULT (CURRENT_TIMESTAMP))";
 
     static final String CREATE_TBL_SETTING = "CREATE TABLE " + TBL_SETTING + "(" +
-            CLM_SETTING_ID + " INTEGER PRIMARY KEY AUTO INCREMENT," +
+            CLM_SETTING_ID + " INTEGER PRIMARY KEY," +
             CLM_ADMIN_EMAIL + " VARCHAR," +
             CLM_HOST + " VARCHAR," +
             CLM_PORT + " VARCHAR," +
@@ -203,22 +291,8 @@ public class DbParams {
             CLM_CREATED + " DATETIME DEFAULT (CURRENT_TIMESTAMP)," +
             CLM_UPDATED + " VARCHAR)";
 
-    static final String CREATE_TBL_LEAVE = "CREATE TABLE " + TBL_LEAVE + "(" +
-            CLM_LOCAL_LEAVE_ID + " DATETIME DEFAULT (CURRENT_TIMESTAMP)," +
-            CLM_SERVER_LEAVE_ID + " INTEGER," +
-            CLM_COMPANY_ID + " INTEGER," +
-            CLM_USER_ID + " INTEGER," +
-            CLM_START_DATE + "  VARCHAR," +
-            CLM_END_DATE + "  VARCHAR," +
-            CLM_STATUS_ID + " INTEGER," +
-            CLM_IS_DELETE + " INTEGER," +
-            CLM_CREATED_BY + " INTEGER," +
-            CLM_CREATED_ON + " DATETIME DEFAULT (CURRENT_TIMESTAMP)," +
-            CLM_UPDATED_BY + " INTEGER," +
-            CLM_UPDATED_ON + " DATETIME DEFAULT (CURRENT_TIMESTAMP))";
-
     static final String CREATE_TBL_ROLE = "CREATE TABLE " + TBL_ROLE + "(" +
-            CLM__ROLE_ID + " INTEGER PRIMARY KEY," +
+            CLM_ROLE_ID + " INTEGER PRIMARY KEY," +
             CLM_NAME + "  VARCHAR," +
             CLM_SORT_ORDER + " INTEGER," +
             CLM_ROLE_GROUP + " INTEGER," +
